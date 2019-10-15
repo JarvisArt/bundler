2c4eede627fa6f96fb0eb2be869b78a88ab1451b
Jarvis <1120886013@qq.com>
2019-10-15 16:31:35 +0800

    feat: 生成HTML文件并注入JS代码

--- bundler.js
+++ bundler.js
@@ -68,5 +68,26 @@ const generateCode = (entry) => {
   `;
 }
 
-const  code = generateCode('./src/index.js'); 
+const  code = generateCode('./src/index.js');
 console.log(code)
+
+const htmlContent = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  </head>
+  <body>
+    <script>${code}</script>
+  </body>
+  </html>
+`
+
+fs.mkdir('./dist', { recursive: true }, (err) => {
+  fs.appendFile('./dist/index.html', htmlContent, (err) => {
+    if (err) throw err;
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import message from './message.js';
 
 console.log(message);
+
+var div = document.createElement('div');
+div.innerHTML = '这是一个div';
+document.body.appendChild(div);
